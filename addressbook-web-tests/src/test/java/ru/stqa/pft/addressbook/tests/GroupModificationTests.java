405dff8352d24be2ab2119b71b8374b18586eebd
Cypumah <sunny.aprelskaya@mail.ru>
2019-09-16 08:21:02 +0300

    Added TestGroupModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Sunny on 15.09.2019.
+ */
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification () {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test7", "7", "77"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
