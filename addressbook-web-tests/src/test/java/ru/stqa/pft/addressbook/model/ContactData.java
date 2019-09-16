7eafa16c6e8f23ddf7b4ae9de11dae20f8cabb8a
Cypumah <sunny.aprelskaya@mail.ru>
2019-09-16 10:22:53 +0300

    Created ContactCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.addressbook.model;
+
+public class ContactData {
+  private final String name;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String company;
+  private final String address;
+  private final String homephone;
+  private final String mobilephone;
+  private final String notes;
+
+  public ContactData(String name, String middlename, String lastname, String nickname, String company, String address, String homephone, String mobilephone, String notes) {
+    this.name = name;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.company = company;
+    this.address = address;
+    this.homephone = homephone;
+    this.mobilephone = mobilephone;
+    this.notes = notes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests {
 
@@ -21,10 +19,7 @@ public class ContactCreationTests {
     System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32\\chromedriver.exe");
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testContactCreation() throws Exception {
     driver.get("http://localhost/addressbook/");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
@@ -35,37 +30,56 @@ public class ContactCreationTests {
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//body")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    driver.findElement(By.linkText("add new")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    initContactCreation();
+    fillContactForm(new ContactData("Ivan", "Ivanov", "Ivanov1", "Iva", "Trim", "st Push, Krasnaya street 8", "64731584", "33342", "privet privet"));
+    submitContactCreaion();
+    returnToContactPage();
+  }
+
+  private void returnToContactPage() {
+    driver.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactCreaion() {
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
     driver.findElement(By.name("firstname")).click();
     driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("Ivan");
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getName());
     driver.findElement(By.name("middlename")).click();
     driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("Ivanov");
+    driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
     driver.findElement(By.name("lastname")).click();
     driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("Ivanov1");
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     driver.findElement(By.name("nickname")).click();
     driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys("Iva");
+    driver.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
     driver.findElement(By.name("company")).click();
     driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys("Trim");
+    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
     driver.findElement(By.name("address")).click();
     driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("st Push, Krasnaya street 8");
+    driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
     driver.findElement(By.name("home")).click();
     driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys("64731584");
+    driver.findElement(By.name("home")).sendKeys(contactData.getHomephone());
     driver.findElement(By.name("mobile")).click();
     driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys("33342");
-    driver.findElement(By.name("theform")).click();
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getMobilephone());
     driver.findElement(By.name("notes")).click();
     driver.findElement(By.name("notes")).clear();
-    driver.findElement(By.name("notes")).sendKeys("privet privet");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
-    driver.findElement(By.id("content")).click();
+    driver.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+  }
+
+  private void initContactCreation() {
+    driver.findElement(By.linkText("add new")).click();
   }
 
   @AfterClass(alwaysRun = true)
