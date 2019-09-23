108cdeeef7c2e1902b9af05f26d716a15eb7183e
Cypumah <sunny.aprelskaya@mail.ru>
2019-09-23 09:32:30 +0300

    Optimized Imports GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,10 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 //import org.testng.annotations.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.security.acl.Group;
 import java.util.HashSet;
 import java.util.List;
 
