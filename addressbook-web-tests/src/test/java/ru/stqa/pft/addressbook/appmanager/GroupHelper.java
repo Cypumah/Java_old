3ceaf496b357ad032188a9e834def6020ef80dac
Cypumah <sunny.aprelskaya@mail.ru>
2019-09-23 12:09:15 +0300

    В тестах Групп оптимизировано сравнение списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -3,12 +3,11 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Sunny on 15.09.2019.
@@ -37,8 +36,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("new"));
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
@@ -49,14 +48,14 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
 
-  public void selectGroup(int index) {
-    driver.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    driver.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public void initGroupModification () {
@@ -78,8 +77,8 @@ public class GroupHelper extends HelperBase {
     return isElementPresent(By.name("selected[]"));
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 //import org.testng.annotations.*;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.Set;
 
 
 public class GroupCreationTests extends TestBase {
@@ -14,22 +14,15 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.goTo().groupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData()
             .withName("test7").withFooter("jfur").withHeader("34");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
- //   int max = 0;
- //   for (GroupData g : after) {
-  //    if (g.getId() > max) {
-  //      max = g.getId();
-  //    }
-  //  }
- //   group.setId(max);
- //   before.add(group);
-  //  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
+    before.add(group);
+    Assert.assertEquals(before, after);
   }
 }
